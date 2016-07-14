0e3e96a07010e6a169fb6f9c0ca471afff011691
Dylan Steck <dylancsteck@gmail.com>
2016-07-14 16:00:26 -0400

    talking to copper

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+require_relative "../models/api.rb"
+class ApplicationController < Sinatra::Base
+  
+set :views, "app/views"
+  set :public, "public"
+
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "dashboard"
+  end
+get '/' do
+erb :index
+end
+
+post '/' do
+  end
+
+  get '/welcome' do
+  end
---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+configure :development do
+  set :database, "sqlite3:db/database.db"
+end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+end
